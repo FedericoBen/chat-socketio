e25417a917c90c4f23128c76b9de2225802819c7
FedericoBen <federicobg95@gmail.com>
2024-09-06 10:36:09 -0500

    [FEATURE] Open cors

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import http from "http";
 import { Server as SocketServer } from "socket.io";
+import cors from 'cors'
 
 const app = express();
 const server = http.createServer(app);
 const io = new SocketServer(server);
+
+app.use(cors())
 
 io.on("connection", (socket) => {
   console.log("client connected");
